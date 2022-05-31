87237a25c76b4ec1d19dd5816c31d8799d0beef0
DavideIsaia <davide.enjoy@hotmail.it>
2022-05-31 12:13:01 +0200

    aggiunto stile elemento calendario

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -20,7 +20,31 @@
     <!-- section founder's citation -->
     <CitCard />
     <!-- section upcoming events -->
-    <section class="events"></section>
+    <section class="events">
+      <div class="container">
+        <div class="calendar">
+          <div class="title bold">Upcoming Events</div>
+          <div class="meeting clearfix">
+            <div class="date">
+              <div class="day">07</div>
+              <div class="month">Jan. 2022</div>
+            </div>
+            <div class="col-right">
+              <h6>Melbourne Coaching</h6>
+              <div>
+                <i class="far fa-clock"></i
+                ><span class="text">9:00 am - 5:00 pm, Jan 7, 2022</span>
+              </div>
+              <div>
+                <i class="fas fa-map-marker-alt"></i
+                ><span class="text">Cambridge, MA 02138, USA</span>
+              </div>
+              <div class="read-more">read more</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
@@ -57,6 +81,29 @@ export default {
           text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
         },
       ],
+      meetings: [
+        {
+          day: "07",
+          month: "Jan. 2022",
+          title: "Melbourne Coaching",
+          hour: "9:00 am - 5:00 pm, Jan 7, 2022",
+          place: "Cambridge, MA 02138, USA",
+        },
+        {
+          day: "11",
+          month: "Jan. 2022",
+          title: "New York Coaching",
+          hour: "9:00 am - 5:00 pm, Jan 11, 2022",
+          place: "Cambridge, MA 02138, USA",
+        },
+        {
+          day: "21",
+          month: "Jan. 2022",
+          title: "London Coaching",
+          hour: "9:00 am - 5:00 pm, Jan 21, 2022",
+          place: "Cambridge, MA 02138, USA",
+        },
+      ],
     };
   },
 };
@@ -87,7 +134,52 @@ main {
     background-size: cover;
     background-position: center;
     margin-top: 5rem;
-    padding: 2rem 0;
+    padding: 6rem 0;
+    .calendar {
+      width: 40%;
+      background-color: $bg-color-04;
+      .title {
+        padding: 1.5rem 3rem;
+        font-size: 1.8rem;
+        border-bottom: 2px solid $bdr-color-03;
+      }
+      .meeting {
+        background-color: $bg-color-04;
+        padding: 3rem 0;
+        border-bottom: 2px solid $bdr-color-03;
+        .date {
+          float: left;
+          padding: 1rem;
+          background-color: $bg-color-08;
+          color: white;
+          text-align: center;
+          margin-left: 3rem;
+          .day {
+            font-size: 1.6rem;
+          }
+          .month {
+            font-family: "Lato", sans-serif;
+            font-size: 0.8rem;
+          }
+        }
+        .col-right {
+          float: left;
+          padding-left: 1rem;
+          text-align: left;
+          line-height: 2rem;
+          h6 {
+            font-size: 1.4rem;
+          }
+          i {
+            margin-right: 1rem;
+            color: $txt-color-08;
+          }
+          span {
+            font-size: 1rem;
+          }
+        }
+      }
+    }
   }
 }
 </style>
